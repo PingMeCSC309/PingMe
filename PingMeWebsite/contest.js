7f782a78db4d603dc5e03e51035aaa0447297e33
Kao Edward Ka-Chung <g3kaoedw@cdf.toronto.edu>
2014-07-24 15:26:48 -0400

    Added Notification and FriendRequest Table in the database

--- PingMeWebsite/contest.js
+++ PingMeWebsite/contest.js
@@ -7,6 +7,7 @@ password	: 'csc309enc',
 database	: 'u928316282_ping'
 });
 connection.connect();
+
 connection.query('CREATE TABLE users'+
 '(userid INT(25) AUTO_INCREMENT,'+
 'username VARCHAR(65) NOT NULL,'+
@@ -18,4 +19,29 @@ if (err) throw err;
 console.log('Table ready');
 });
 
+connection.query('CREATE TABLE friendRelations'+
+'(userid1 INT(25),'+
+'userid2 INT(25)'+
+'inviteResponse INT(25));',function(err,rows,fields){
+
+if (err) throw err;
+console.log('Table ready');
+});
+
+connection.query('CREATE TABLE globalNotifications'+
+'(notificationid INT(25) AUTO_INCREMENT,'+
+'recipientid INT(25),'+
+'senderid INT(25),'+
+'type INT(255));',function(err,rows,fields){
+
+if (err) throw err;
+console.log('Table ready');
+});
+
+
+
+
+
+
 connection.end();
+
